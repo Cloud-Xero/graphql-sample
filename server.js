bf7fd6c9e8fd3c9e21b6f29b853198a9055ce258
Cloud-Xero <hiropon201511@gmail.com>
2022-11-06 01:29:13 +0900

    feat: object type

--- server.js
+++ server.js
@@ -6,9 +6,9 @@ const { buildSchema } = require("graphql");
 // スキーマはあくまで定義のみで実際のデータ操作は行わない
 const schema = buildSchema(`
   type RandomDie {
-    numberSides: Int!
+    numSides: Int!
     rollOnce: Int!
-    roll(numRolls: Int): [Int]
+    roll(numRolls: Int!): [Int]
   }
 
   type Query {
@@ -16,15 +16,29 @@ const schema = buildSchema(`
   }
 `);
 
-// リゾルバ関数（特定のフィールドのデータを返す関数であり、実際のデータ操作を行う部分）
-const root = {
-  // クライアント側のクエリから引数の値を受け取る
-  rollDice: ({ numDice, numSides }) => {
+// リゾルバ関数内の処理をクラス化することも可能
+class RandomDie {
+  constructor(numSides) {
+    this.numSides = numSides;
+  }
+
+  rollOnce() {
+    return 1 + Math.floor(Math.random() * this.numSides);
+  }
+
+  roll({ numRolls }) {
     let output = [];
-    for (let i = 0; i < numDice; i++) {
-      output.push(1 + Math.floor(Math.random() * (numSides || 6)));
+    for (let i = 0; i < numRolls; i++) {
+      output.push(this.rollOnce());
     }
     return output;
+  }
+}
+
+// リゾルバ関数（特定のフィールドのデータを返す関数であり、実際のデータ操作を行う部分）
+const root = {
+  getDie: ({ numSides }) => {
+    return new RandomDie(numSides || 6);
   },
 };
 
