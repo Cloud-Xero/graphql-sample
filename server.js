5edb6b96cb45dde5c7c8520d85b85e15c7d374df
Cloud-Xero <hiropon201511@gmail.com>
2022-11-06 01:30:27 +0900

    feat: mutation and input types

--- server.js
+++ server.js
@@ -5,40 +5,64 @@ const { buildSchema } = require("graphql");
 // GraphQLスキーマ言語を記述してスキーマを構築する
 // スキーマはあくまで定義のみで実際のデータ操作は行わない
 const schema = buildSchema(`
-  type RandomDie {
-    numSides: Int!
-    rollOnce: Int!
-    roll(numRolls: Int!): [Int]
+  input MessageInput {
+    content: String
+    author: String
+  }
+
+  type Message {
+    id: ID!
+    content: String
+    author: String
   }
 
   type Query {
-    getDie(numSides: Int): RandomDie
+    getMessage(id: ID!): Message
+  }
+
+  type Mutation {
+    createMessage(input: MessageInput): Message
+    updateMessage(id: ID!, input: MessageInput): Message
   }
 `);
 
-// リゾルバ関数内の処理をクラス化することも可能
-class RandomDie {
-  constructor(numSides) {
-    this.numSides = numSides;
-  }
-
-  rollOnce() {
-    return 1 + Math.floor(Math.random() * this.numSides);
-  }
-
-  roll({ numRolls }) {
-    let output = [];
-    for (let i = 0; i < numRolls; i++) {
-      output.push(this.rollOnce());
-    }
-    return output;
+class Message {
+  constructor(id, { content, author }) {
+    this.id = id;
+    this.content = content;
+    this.author = author;
   }
 }
 
+// データの入れ物
+let fakeDatabase = {};
+
 // リゾルバ関数（特定のフィールドのデータを返す関数であり、実際のデータ操作を行う部分）
 const root = {
-  getDie: ({ numSides }) => {
-    return new RandomDie(numSides || 6);
+  getMessage: ({ id }) => {
+    if (!fakeDatabase[id]) {
+      throw new Error("no message exists with id" + id);
+    }
+
+    return new Message(id, fakeDatabase[id]);
+  },
+
+  createMessage: ({ input }) => {
+    // ランダムなIdを生成
+    var id = require("crypto").randomBytes(10).toString("hex");
+
+    fakeDatabase[id] = input;
+
+    return new Message(id, input);
+  },
+
+  updateMessage: ({ id, input }) => {
+    if (!fakeDatabase[id]) {
+      throw new Error("no message exists with id" + id);
+    }
+    // 古いデータの書き換え
+    fakeDatabase[id] = input;
+    return new Message(id, input);
   },
 };
 
